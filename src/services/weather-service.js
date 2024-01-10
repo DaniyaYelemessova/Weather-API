ac57ea172ed7807491d8ac8ef4d81b3eab052d8f
Daniya Yelemessova <daniya@collingsmail.com>
2024-01-09 21:40:44 -0500

    Add separation off logic

---
+++ src/services/weather-service.js
@@ -0,0 +1,18 @@
+export default class WeatherService{
+  static getWeather(city){
+    return fetch (`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.OPEN_WEATHER_API_KEY}&units=metric`)
+      .then(function(response) {
+        if(!response.ok) {
+          const errorMessage = `${response.status} ${response.statusText}`;
+          throw new Error(errorMessage);
+        } else {
+          return response.json();
+        }
+      })
+      .catch(function(error) {
+        return error;
+      });
+  }
+}
+
+
